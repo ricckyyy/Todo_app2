75bc461456eace7ed4daf05d01c53c58a84c1dc6
R-T <csri16001@gmail.com>
2023-09-26 10:42:51 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Riki T on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
