e4a72e1b6f648239e18d4aac6235e0b1172ff50e
R-T <csri16001@gmail.com>
2024-01-24 21:46:59 +0900

    画像表示

--- Todo/CategoryImage.swift
+++ Todo/CategoryImage.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct CategoryImage: View {
+    @State private var showFirstDialog = false
+    @State private var showSecondDialog = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "moon.stars")
+            .resizable()
+            .scaledToFit()
+            .padding(2.0)
+            .frame(width: 30,height: 30)
+            
     }
 }
 
-struct CategoryImage_Previews: PreviewProvider {
+struct CategoryImage_Pre: PreviewProvider {
     static var previews: some View {
         CategoryImage()
     }
 }
+
